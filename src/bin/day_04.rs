5206066424d75088f24d1b0b86a2658c9ccd7c18
TyLanger <ty.langer16@gmail.com>
2024-05-05 04:46:18 -0600

    Day 04 part 2
    Easy enough
    When you win a card, you gain a copy of the next card in the sequence
    This doesn't change the math much. Instead of adding the score, you're
    adding the number of copies to the x next cards.
    It's not like you have to simulate more cards. If you have 8 copies of a
    card, check for winners once and then multiply stuff by 8 or whatever.
    
    Copies array
    Keeps track of how many copies of the next 10 cards you have. Only need to
    keep track of the next 10 because you have 10 winning numbers so the most
    about of winning numbers you can get is 10.
    So use an 11 size array for current + 10 next instead of a 200+ size array
    
    how_many_matches
    extract this function from part 1 so it's easy to resuse for p2
    Even though I copy-pasted the parsing code
    
    modulo_math
    sanity check that I was using modulo correctly

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -5,6 +5,12 @@ fn main() {
 
     println!("{}", part_1(&input));
     // 24175
+    println!("{}", part_2(&input));
+    // 18731808 too low
+    // whoops, the copies array should be 11, not 10
+    // needs to be 11 to fit the possible 10 matches
+    // you are given 10 winnng numbers. It's possible they all match
+    // 18846301 correct
 }
 
 fn part_1(input: &str) -> u32 {
@@ -28,26 +34,11 @@ fn part_1(input: &str) -> u32 {
 
         // print!("Winners: {:?}", &winners);
         // println!("  Numbers: {:?}", &numbers);
+        let num_hits = how_many_matches(&winners, &numbers);
 
-        let mut num_hits = 0;
-        // when checking a new winning number, start where you left off
-        // in the number we have. They are sorted after all.
-        let mut number_index = 0;
-        for w in winners {
-            for i in number_index..numbers.len() {
-                if w == numbers[i] {
-                    num_hits += 1;
-                    number_index = i;
-                    // assuming no duplicates in the numbers we have
-                    break;
-                } else if w < numbers[i] {
-                    number_index = i;
-                    break;
-                }
-            }
-        }
         if num_hits > 0 {
-            sum += 1 << (num_hits -1);
+            // can do doubles by shifting
+            sum += 1 << (num_hits - 1);
         }
         // sum += match num_hits {
         //     0 => 0,
@@ -72,6 +63,72 @@ fn part_1(input: &str) -> u32 {
     sum
 }
 
+fn part_2(input: &str) -> u32 {
+    let lines: Vec<&str> = input.lines().collect();
+
+    let mut sum = 0;
+    // keep track of how many copies of the associated card you have
+    // only need to keep track of the next 10, don't need to remember
+    // how many copies of all 200+ cards in the input.
+    // 11 because current + 10 cards ahead. It's winners.len() + 1
+    let mut copies = vec![0; 11];
+    let mut copy_index: usize = 0;
+
+    for line in lines {
+        let split: Vec<&str> = line.split("|").collect();
+        let mut winners: Vec<u32> = split[0]
+            .split(" ")
+            .filter_map(|x| x.parse::<u32>().ok())
+            .collect();
+        let mut numbers: Vec<u32> = split[1]
+            .split(" ")
+            .filter_map(|x| x.parse::<u32>().ok())
+            .collect();
+
+        winners.sort();
+        numbers.sort();
+
+        let num_hits = how_many_matches(&winners, &numbers);
+        let old_index = copy_index;
+        copy_index = (copy_index + 1) % copies.len();
+
+        // the next (num_hits) cards get 1 extra card
+        for i in 0..num_hits {
+            let overlap: usize = (copy_index + usize::try_from(i).unwrap()) % copies.len();
+            copies[overlap] += 1 + copies[old_index];
+            sum += 1 + copies[old_index];
+        }
+        // count the real card
+        sum += 1;
+        // reset and reuse this circular array
+        copies[old_index] = 0;
+    }
+
+    sum
+}
+
+fn how_many_matches(winners: &Vec<u32>, numbers: &Vec<u32>) -> u32 {
+    let mut num_hits = 0;
+    // when checking a new winning number, start where you left off
+    // in the number we have. They are sorted after all.
+    let mut number_index = 0;
+    for w in winners {
+        for i in number_index..numbers.len() {
+            if *w == numbers[i] {
+                num_hits += 1;
+                number_index = i;
+                // assuming no duplicates in the numbers we have
+                break;
+            } else if *w < numbers[i] {
+                number_index = i;
+                break;
+            }
+        }
+    }
+
+    num_hits
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -87,5 +144,17 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
     #[test]
     fn part_1_works() {
         assert_eq!(13, part_1(&BASIC_INPUT_DAY_04));
+    }
+
+    #[test]
+    fn part_2_works() {
+        assert_eq!(30, part_2(&BASIC_INPUT_DAY_04));
+    }
+
+    #[test]
+    fn modulo_math() {
+        assert_eq!(3, 3 % 10);
+        assert_eq!(3, 13 % 10);
+        assert_eq!(0, 10 % 10);
     }
 }
