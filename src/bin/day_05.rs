64123f6e4c89df2d54099034e5568150f0b4f600
TyLanger <ty.langer16@gmail.com>
2024-05-05 23:35:17 -0600

    Day 05 part 2
    It's pretty much the same as p1, but instead of seed numbers, you operate
    on seed ranges.
    You start with your 10 seed ranges from a-b and then you transform these
    ranges through all of the maps to get all the values you could possibly
    get
    
    Turning the input into transform_maps was the same as p1
    Now I need to check for the 6 types of range collisions and then cut up my
    seed ranges into smaller and smaller pieces as it goes.
    With equalities, there are more than 6 collision types, but I don't know
    how many.
    
    Bugs
    Wherever is has println(broken) is where I was testing to see where my
    ranges got out of order. They should all be (2, 6), never (6, 2), but it
    was happening somewhere.
    I found it in broken 1.5 when the start of the seed range and the start of
    the transform_map were equal.
    I don't know if this is the only place it breaks if they're equal or just
    the only place in my input.
    In all of my collision, I'm not sure if it's completely correct or if
    there's still an off-by-one error hiding.
    
    Structure/Architecture
    It's kinda wonky same as p1
    I could probably move some stuff around and extract stuff to functions to
    make it look nicer and easier to read.
    I am happy ith the A--B C--D graphics. They're pretty clear. Except when
    they're equal. I don't quite know how that works.
    
    check_again
    A necessary evil and it works, but maybe it can be more clear
    I don't think rust lets me modify i, j, k within the loop to change how
    many times it runs. It sets how many times it will run at the start and i,
    j, k are just counters. I can make the mut and do what I want with them,
    but it won't actually change how many times the loop runs.
    All that to say, I can't insert the range into ranges and have to use the
    check_again workaround. It works fine enough I guess.
    
    shortcut_index
    Should I just set this at the start of the loop? That way I wouldn't have
    to do k vs k+1.

--- src/bin/day_05.rs
+++ src/bin/day_05.rs
@@ -7,6 +7,10 @@ fn main() {
     // 1881211815 too high
     // whoops forgot to sort before output
     // 403695602 correct
+    println!("{}", part_2(&input));
+    // 39360041 too low
+    // Wasn't handling values being equal correctly
+    // 219529182 correct
 }
 
 #[derive(Debug)]
@@ -68,7 +72,7 @@ fn part_1(input: &str) -> u64 {
                         // ffs I missed this break
                         break;
                     } else {
-                        shortcut_index = k+1;
+                        shortcut_index = k + 1;
                         // shortcut_index = k;
                         // bigger than ths current map; check the next one
                         // update the shortcut_index so subsequent seed_numbers
@@ -110,6 +114,240 @@ fn part_1(input: &str) -> u64 {
     // starts storing seeds, then soils, then fertilizer, etc.
     // all in the same seeds array
     seeds[0]
+}
+
+fn part_2(input: &str) -> u64 {
+    // instead of having 4 seeds, I now have 27 seed numbers
+    // can I still brute force this?
+    // the real input will have maybe 10b seed numbers
+    // there are 20 9-digit numbers
+    // so 10 lengths of ~1b seeds
+
+    // only need 2 seed numbers at each step
+    // one that gets changed and another that doesn't
+    // well, more than 2
+    // If one transform is from 30 to 60,
+    // I want to test what happens if I pass in 30 and what happens if I pass in 60
+    // don't really need to check 31, 32, 33, etc
+    // but then I need to do this minimax for each step
+    // say step 1 is 30-60 gets turned into 80-110
+    // then stp 2 is 90-95 gets turned to 5-10
+    // computing for 30 and 60 doesn't get the min
+    // I'd need to look ahead and do 90 to get to 5
+    // this seems hard
+    // how would I represent this?
+    // I have seed numbers from 20-100
+    // they can become 20-30, 30-60 becomes 80-110, 60-80, 80-110 becomes x-y
+    // 90-95 -> 5-10
+    // 20-30, 80-90, 90-95 becomes 5-10, 95-110, 60-80, x-y
+    // seems possible
+    // take a range from a-b, then possibly split it into a-c, c-d, d-b
+    // maybe merge them?
+    // I have 5-10, 15-30
+    // next test is 0-40 becomes 70-110
+    // Then I have 75-80, 85-100 with no merging?
+    // merging is probably unlikely
+    // double split
+    // I have 40-80
+    // next test is 45-50, 70-75
+    // step 1: 40-45, 45-50, 50-80
+    // step 2:               50-70, 70-75, 75-80
+    // after: 40-45, 45-50, 50-70, 70-75, 75-80
+    // how do I loop this?
+    // how do I store the data?
+    // 50-80 is an intermediary that needs to be checked twice. How do I determine these?
+
+    // this is what I was thinking about in p1 about doing it backwards, starting with the location
+    // I could start at 0 and then work backwards and test to see if there's a valid path to a seed
+    // i would need to do 1b iterations probably. The solution for p1 was ~1b
+
+    // can I add all the maps together?
+    // form my own map
+    // step 1 shifts 9 to 2
+    // step 2 shifts 2 to 4
+    // can be simplified to 9 to 4
+
+    // seed ranges
+    // [(a,b), (c,d), (e,f), etc]
+    // go through each map and split these up
+
+    let lines: Vec<&str> = input.lines().collect();
+
+    let seeds: Vec<u64> = lines[0]
+        .split(" ")
+        .filter_map(|x| x.parse::<u64>().ok())
+        .collect();
+
+    // inclusive
+    // 50 98 2 would be 98,99 or 50,51
+    let mut ranges: Vec<(u64, u64)> = Vec::new();
+
+    let half_seeds = seeds.len() / 2;
+
+    for i in 0..half_seeds {
+        ranges.push((seeds[i * 2], seeds[i * 2] + seeds[i * 2 + 1] - 1));
+    }
+
+    ranges.sort();
+
+    let mut transform_maps: Vec<TransformMap> = Vec::new();
+    let mut skip_line = 2;
+
+    let mut check_again: Vec<(u64, u64)> = Vec::new();
+
+    for i in 2..lines.len() {
+        // is this line empty or is this the last line
+        if lines[i].is_empty() || i == (lines.len() - 1) {
+            // the maps are done, do the logic
+            transform_maps.sort_by(|a, b| a.from.partial_cmp(&b.from).unwrap());
+            // println!("transform maps after sort: {:?}", &transform_maps);
+            let mut shortcut_index = 0;
+            let mut next_ranges: Vec<(u64, u64)> = Vec::new();
+
+            for j in 0..ranges.len() {
+                for k in shortcut_index..transform_maps.len() {
+                    let mut a = ranges[j].0;
+                    let mut b = ranges[j].1;
+                    let c = transform_maps[k].from;
+                    let d = transform_maps[k].from + transform_maps[k].len - 1;
+                    if check_again.len() > 0 {
+                        a = check_again[0].0;
+                        b = check_again[0].1;
+                        check_again.clear();
+                        // println!("Check again");
+                        // println!("i, j, k: {}, {}, {}", i, j, k);
+                    }
+                    // does this range intersect this map?
+                    if b < c {
+                        // range: 1-3       A--B
+                        // map: 5-7              C--D
+                        // no collision
+                        // I can ignore it
+                        next_ranges.push(ranges[j]);
+                        break;
+                    } else if a > d {
+                        // range 6-10           A--B
+                        // map: 1-4       C--D
+                        // no collision
+                        // try again with the next transform_map
+                        if k == transform_maps.len() - 1 {
+                            next_ranges.push(ranges[j]);
+                            shortcut_index = k;
+                        } else {
+                            shortcut_index = k + 1;
+                        }
+                    } else {
+                        // some kind of collision
+
+                        // range: 30-40        A--B
+                        // map: 10-60         C-----D
+                        if a >= c && b <= d {
+                            // 98 + 50 - 98 = 50
+                            // 99 + 50 - 98 = 51
+                            let new_a = a + transform_maps[k].to - transform_maps[k].from;
+                            let new_b = b + transform_maps[k].to - transform_maps[k].from;
+                            if new_a > new_b {
+                                println!("broken 1 a: {}, b: {}, c: {}, d: {}", a, b, c, d);
+                                println!("i, j, k: {}, {}, {}", i, j, k);
+                            }
+                            next_ranges.push((new_a, new_b));
+                            shortcut_index = k;
+                            break;
+                        }
+                        // range: 20-60        A------B
+                        // map: 30-40            C--D
+                        else if a <= c && b >= d {
+                            // push a-c, c-d, but not d-b. It might match another map
+                            if a > c {
+                                println!("broken 1.5 a: {}, b: {}, c: {}, d: {}", a, b, c, d);
+                                println!("i, j, k: {}, {}, {}", i, j, k);
+                            }
+                            // is this the only case where they can be exact?
+                            // or just the only case in my input?
+                            if a != c {
+                                next_ranges.push((a, c - 1));
+                            }
+
+                            let new_c = transform_maps[k].to;
+                            let new_d = transform_maps[k].to + transform_maps[k].len - 1;
+                            if new_c > new_d {
+                                println!("broken 2");
+                            }
+                            next_ranges.push((new_c, new_d));
+                            //  I don't think I can just add to ranges
+                            // while I'm already looping 0..ranges.len()
+                            check_again.push((d + 1, b));
+                            // don't break
+                            // continue to the next transform_map
+                            // for collisions with check_again
+                            // it has to be the next one or none of them.
+                        }
+                        // range: 1-10         A--B
+                        // map: 5-20             C--D
+                        else if a <= c && b >= c {
+                            if a > c-1 {
+                                println!("broken 2.5 a: {}, b: {}, c: {}, d: {}", a, b, c, d);
+                                println!("i, j, k: {}, {}, {}", i, j, k);
+                            }
+                            next_ranges.push((a, c - 1));
+
+                            let new_c = transform_maps[k].to;
+                            let new_b = b + transform_maps[k].to - transform_maps[k].from;
+                            if new_c > new_b {
+                                println!("broken 3");
+                            }
+                            next_ranges.push((new_c, new_b));
+                            break;
+                        }
+                        // range: 10-20         A--B
+                        // map: 5-15          C--D
+                        else if a >= c && b >= d {
+                            let new_a = a + transform_maps[k].to - transform_maps[k].from;
+                            let new_d = transform_maps[k].to + transform_maps[k].len - 1;
+                            if new_a > new_d {
+                                println!("broken 4");
+                            }
+                            next_ranges.push((new_a, new_d));
+                            check_again.push((d + 1, b));
+                        } else {
+                            println!("I missed a case j:{}, k:{}", j, k);
+                        }
+                    }
+                }
+            }
+
+            // I want to skip the next line
+            // I know the next line after a blank will be
+            // soil-to-fertilizer map:
+            skip_line = i + 1;
+            // clear the old maps
+            transform_maps.clear();
+
+            // println!("Ranges: {:?}", &ranges);
+            // println!("Next ranges: {:?}", &next_ranges);
+
+            ranges = next_ranges.clone();
+            ranges.sort();
+        } else if i == skip_line {
+            // skip the "seel-to-soil map:" lines
+            // they come after blank lines
+            continue;
+        } else {
+            let triple: Vec<u64> = lines[i]
+                .split(" ")
+                .filter_map(|x| x.parse::<u64>().ok())
+                .collect();
+            let map = TransformMap {
+                to: triple[0],
+                from: triple[1],
+                len: triple[2],
+            };
+            transform_maps.push(map);
+        }
+    }
+
+    ranges.sort();
+    ranges[0].0
 }
 
 #[cfg(test)]
@@ -154,6 +392,11 @@ humidity-to-location map:
     #[test]
     fn part_1_works() {
         assert_eq!(35, part_1(&BASIC_INPUT_DAY_05));
+    }
+
+    #[test]
+    fn part_2_works() {
+        assert_eq!(46, part_2(&BASIC_INPUT_DAY_05));
     }
 
     #[test]
